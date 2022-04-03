8ff2f241bf97bdcfb9f0e418a188f005cb1d73f5
Dawid Grobert <dawidgrobert@gmail.com>
2022-04-03 21:24:11 +0200

    Add basic type of node scene
    This is a container for drawable game objects. It allows to keep
    them together as one drawable element on the stage. Moving such an
    element also moves the elements it contains.

---
+++ FlapANN/src/Nodes/NodeScene.cpp
@@ -0,0 +1,108 @@
+#include "pch.h"
+#include "Nodes/NodeScene.h"
+
+#include <algorithm>
+#include <cassert>
+#include <functional>
+
+NodeScene::NodeScene() :
+	mPinnedNodes(),
+	mParent(nullptr)
+{
+}
+
+void NodeScene::pinNode(Node node)
+{
+	// Now this Scene is mParent of given node
+	node->mParent = this;
+
+	// So lets add it into mPinnedNodes of this Scene
+	mPinnedNodes.push_back(std::move(node));
+
+}
+
+NodeScene::Node NodeScene::unpinNode(const NodeScene& node_scene)
+{
+	// The address of given NodeScene have to match one of the addresses inside our vector of mPinnedNodes
+	auto foundNode = std::find_if(mPinnedNodes.begin(), mPinnedNodes.end(), [&node_scene](Node& pinned_node) -> bool { return pinned_node.get() == &node_scene; });
+
+	// Make sure that we found such a Node to unpin
+	assert(foundNode != mPinnedNodes.end());
+
+	// Now as we found the given NodeScene we must unpin it from our vector
+	// First take the ownership from the vector
+	auto stolenNode = std::move(*foundNode);
+
+	// std::move makes sure it is in valid state to run destructor, so we delete it
+	mPinnedNodes.erase(foundNode);
+	
+	// Right now the stolen_node is not part of this Scene anymore.
+	// We may remove its "parentness"
+	stolenNode->mParent = nullptr;
+
+	return stolenNode;
+}
+
+sf::Vector2f NodeScene::getAbsolutePosition() const
+{
+	// To do this we have to add all transforms till the top of the hierarchy.
+	sf::Transform transform; //sf::Transform::Identity;
+	for (const auto* node = this; node != nullptr; node = node->mParent)
+		transform = node->getTransform() * transform;
+	
+	// By applying this transform on empty Vector2f we move it to the desired position
+	return transform * sf::Vector2f();
+}
+
+void NodeScene::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	// We want to draw this object in relation to the mParent
+	// states.transform stores information about mParent scene render position
+	// by multiplying it by our getTransform() position we get position related
+	// to our mParent
+
+	states.transform *= getTransform(); // Absolute world transform is result of this operation
+
+	// As our object is in the right position, we can finally draw it
+	drawThis(target, states);
+
+	// Right after we did this, we can forward drawing to the nodes lower in the hierarchy.
+	// This way we will draw all nodes to the screen starting from the root.
+	// Also it will help us to maintain the order of which should be in front or in the back of the screen
+	for (const auto& pinnedNode : mPinnedNodes)
+		pinnedNode->draw(target, states);
+}
+
+void NodeScene::drawThis(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	// Nothing here
+}
+
+void NodeScene::update(sf::Time deltaTime)
+{
+	// We starting by updating this object
+	updateThis(deltaTime);
+
+	// And then we update all mPinnedNodes -- which updates all nodes pinned to them
+	for (auto& pinnedNode : mPinnedNodes)
+		pinnedNode->update(deltaTime);
+}
+
+void NodeScene::updateThis(sf::Time deltaTime)
+{
+	// Nothing here
+}
+
+void NodeScene::handleEvents(const sf::Event& event)
+{
+	// Handle event in this node
+	handleThisEvents(event);
+
+	// And next pass the event to all pinned nodes
+	for (auto& pinnedNode : mPinnedNodes)
+		pinnedNode->handleEvents(event);
+}
+
+void NodeScene::handleThisEvents(const sf::Event& event)
+{
+}
---
+++ FlapANN/src/Nodes/NodeScene.h
@@ -0,0 +1,166 @@
+#ifndef NODESCENE_H
+#define NODESCENE_H
+
+#include <list>
+#include <memory>
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/Transformable.hpp>
+#include <SFML/System/NonCopyable.hpp>
+#include <SFML/System/Time.hpp>
+#include <SFML/Window/Event.hpp>
+
+
+/**
+ * \brief Scene on which objects and other pinned scenes will be drawn.
+ *
+ * Individual nodes (objects inside the game) can be parents of other nodes.
+ * Thus, moving the mParent will make the relativistic position of the nodes
+ * (children) pinned to it also to change. This makes it very easy to create
+ * any hierarchies and scenes within the game. Nodes that are pinned to a
+ * mParent node at the time of drawing receive **transform** of node they
+ * are pinned to, so that they can draw themselves relative to their mParent.
+ *
+ * We can say that the way the nodes are executed resembles "Depth-first search".
+ * It starts at the root node, and it explores as far as possible along each branch
+ * before backtracking. This also means that objects higher up in the hierarchy
+ * (parents) will be drawn on the screen underneath their pinned nodes (children).
+ *
+ *               +-----------+
+ *               |(Root) Node|
+ *               +-----+-----+
+ *                     |
+ *     +---------+-----+------------+
+ *     |         |                  |
+ *   +-v--+    +-v--+             +-v--+
+ *(1)|Node| (2)|Node|             |Node|(7)
+ *   +----+    +-+--+             +-+--+
+ *               |                  |
+ *             +-v--+             +-v--+
+ *          (3)|Node|             |Node|(8)
+ *       +-----+-+--+-----+       +-+--+
+ *       |       |        |         |
+ *     +-v--+  +-v--+  +--v-+     +-v--+
+ *     |Node|  |Node|  |Node|     |Node|(9)
+ *     +----+  +----+  +----+     +----+
+ *       (4)     (5)     (6)
+ *
+ * It derives from sf::Drawable as it is suppossed to be drawn on the screen.
+ * It derives from sf::Transformable which gives all members related with position, rotation and scale
+ * It derives from sf::NonCopyable as NodeScene like this should not be copied (it may give many problems in current state)
+ * This make copy constructor/assignment to be deleted
+ */
+class NodeScene : public sf::Drawable, public sf::Transformable, private sf::NonCopyable
+{
+public:
+	using Node = std::unique_ptr<NodeScene>;
+
+	NodeScene();
+	virtual ~NodeScene() override = default;
+
+	/**
+	 * \brief Steals ownership, and puts it into the vector of pinned nodes
+	 * \param node node to steal
+	 *
+	 * After this function, the "node" is a pinned node to this node.
+	 * The node that uses this function becomes the mParent of the "node".
+	 */
+	void pinNode(Node node);
+
+	/**
+	 * \brief Removes this NodeScene from pinnedNodes and returns it
+	 * \param node node to unpin
+	 * \return unique_ptr to the unpinned node
+	 *
+	 * Removes this NodeScene from pinned_Nodes and returns it.
+	 * After this operation, this node is no longer a mParent "node".
+	 */
+	Node unpinNode(const NodeScene& node);
+
+	// Returns absolute position in the game plane
+	
+	/**
+	 * \brief A function that calculates the absolute position of this node.
+	 * \return Returns the absolute position of the node object on the screen.
+	 */
+	sf::Vector2f getAbsolutePosition() const;
+
+	
+	// ====== Drawing Scenes ====== //
+
+	/**
+	 * \brief Draws this node, and all pinned nodes to the passed target.
+	 * \param target where this node, and all pinned nodes should be drawn to.
+	 * \param states provides information about rendering process (transform, shader, blend mode)
+	 *
+	 * This function is provided inside sf::Drawable. Thanks to this if we pass this object to
+	 * sf::RenderWindow::draw(), then it will implicitly call this function to draw it!
+	 */
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const override final;
+
+	
+	/**
+	 * \brief Draws only this node to the passed target
+	 * \param target where it should be drawn to
+	 * \param states provides information about rendering process (transform, shader, blend mode)
+	 */
+	virtual void drawThis(sf::RenderTarget& target, sf::RenderStates states) const;
+
+
+	// ====== Updating Scenes ====== //
+
+	/**
+	 * \brief Updates the object logic of this node and the pinned nodes.
+	 * \param deltaTime the time that has passed since the game was last updated.
+	 *
+	 * In comparison to draw() this function is not derived. It is used to update all mPinnedNodes
+	 */
+	virtual void update(sf::Time deltaTime);
+
+	/**
+	 * \brief Updates the logic of this node only
+	 * \param deltaTime the time that has passed since the game was last updated
+	 *
+	 * Works analogues to the drawThis(), updates all things related to itself
+	 */
+	virtual void updateThis(sf::Time deltaTime);
+
+	
+	// ====== Handling events ====== //
+	
+	/**
+	 * \brief It takes input (event) from the user and interprets it, then passes it on to the pinned nodes
+	 * \param event user input
+	 */
+	void handleEvents(const sf::Event& event);
+
+	/**
+	 * \brief It takes input (event) from the user and interprets it
+	 * \param event user input
+	 */
+	virtual void handleThisEvents(const sf::Event& event);
+
+private:
+
+	/**
+	 * \brief A list holding all the nodes attached (children) to this node
+	 *
+	 * It was vector before, but I had to change it to the list, as some object
+	 * inside range-for loop of this mPinnedNodes started pin its nodes to this
+	 * global mPinnedNodes and that was risky that mPinnedNodes may reallocate
+	 * during its iteration of the range-for loop
+	 *
+	 * What objects started to pin some object to the root_node?
+	 * Actually bullets, I wanted them to be global, thus I put
+	 * them in the root node
+	 */
+	std::vector<Node> mPinnedNodes;
+
+	/**
+	 * \brief Parent node of this node.
+	 *
+	 * Nullptr in case of there is no mParent.
+	 */
+	NodeScene* mParent;
+};
+
+#endif
