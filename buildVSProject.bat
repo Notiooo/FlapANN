4af0bead4c18c7820849770f55c9882945b730d2
Dawid Grobert <dawidgrobert@gmail.com>
2022-04-03 20:55:21 +0200

    Add project files and vendors
    I plug libraries such as ImGui and SFML into the project. Everything
    hooked up to premake 5.

---
+++ buildVSProject.bat
@@ -0,0 +1,2 @@
+call FlapANN\vendor\bin\premake\premake5.exe vs2019
+PAUSE
---
+++ premake5.lua
@@ -0,0 +1,122 @@
+workspace "FlapANN"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+    filter "configurations:Debug"
+        defines "_DEBUG"
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "_RELEASE"
+        runtime "Release"
+        optimize "On"
+        
+    
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "FlapANN"
+    location "FlapANN"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{wks.name}")
+    objdir("bin-int/" .. outputdir .. "/%{wks.name}")
+
+    pchheader "pch.h"
+    pchsource "%{wks.name}/src/pch.cpp"
+
+    files
+    {
+        -- main source files
+        "%{wks.name}/src/**.h",
+        "%{wks.name}/src/**.cpp",
+
+        -- vendors
+        "%{wks.name}/vendor/src/**.cpp",
+        "%{wks.name}/vendor/src/**.c"
+    }
+
+    includedirs
+    {
+        "%{wks.name}/vendor/include",
+        "%{wks.name}/src"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        systemversion "latest"
+
+        defines
+        {
+            "SFML_STATIC",
+        }
+
+        libdirs
+        {
+            "%{wks.name}/vendor/lib/imgui/windows",
+            "%{wks.name}/vendor/lib/imgui-sfml/windows",
+            "%{wks.name}/vendor/lib/SFML/windows"
+        }
+
+        links
+        {
+            -- SFML
+            "opengl32",
+            "freetype",
+            "winmm",
+            "gdi32",
+            "openal32",
+            "flac",
+            "vorbisenc",
+            "vorbisfile",
+            "vorbis",
+            "ogg"
+        }
+
+        postbuildcommands
+        {
+            ('{COPYFILE} ../%{wks.name}/vendor/lib/SFML/windows/openal32.dll %{cfg.buildtarget.bundlepath}'),
+            ('{COPYDIR} ../%{wks.name}/resources %{cfg.buildtarget.bundlepath}/resources')
+        }
+
+        filter {"configurations:Debug", "system:windows"}
+            links { "sfml-audio-s-d", "sfml-graphics-s-d", "sfml-network-s-d", "sfml-system-s-d", "sfml-window-s-d" }
+
+        filter {"configurations:Release", "system:windows"}
+            links { "sfml-audio-s", "sfml-graphics-s", "sfml-network-s", "sfml-system-s", "sfml-window-s" }
+
+
+--    filter "system:linux"
+--        cppdialect "C++17"
+--
+--        libdirs
+--        {
+--            "%{wks.name}/vendor/lib/box2d/linux",
+--            "%{wks.name}/vendor/lib/gtest/linux",
+--            "%{wks.name}/vendor/lib/SFML/linux",
+--            "%{wks.name}/vendor/lib/poly2tri/linux",
+--            "%{wks.name}/vendor/lib/clipper/linux"
+--        }
+--
+--        links { "glew32s", "sfml-audio", "sfml-graphics", "sfml-network", "sfml-system", "sfml-window" }
+--        linkoptions { '-Wl,-rpath=./libs' }
+--        
+--        postbuildcommands
+--        {
+--            ('{MKDIR} %{cfg.buildtarget.bundlepath}/libs'),
+--            ('{COPYFILE} ../%{wks.name}/vendor/lib/SFML/linux/* %{cfg.buildtarget.bundlepath}/libs'),
+--            ('{COPYDIR} ../%{wks.name}/resources %{cfg.buildtarget.bundlepath}')
+--        }
+        
+        filter "files:**.c"
+            flags {"NoPCH"}
+
+        filter "files:FlapANN/vendor/src/**.cpp"
+            flags {"NoPCH"}
+
